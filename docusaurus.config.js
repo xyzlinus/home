371f48c5b5d05c2474f0c5b468f710f8c09eba8d
xyzlinus <52217114+xyzlinus@users.noreply.github.com>
2023-10-01 12:22:38 +0200

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -43,14 +43,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/xyzlinus/Docusaurus/tree/main/',
+            'https://github.com/xyzlinus/home/tree/main/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/xyzlinus/Docusaurus/tree/main/',
+            'https://github.com/xyzlinus/home/tree/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
